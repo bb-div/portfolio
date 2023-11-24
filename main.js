799a29773e866236b9b5164568b0d775f14383c8
Boris Ballhaus <104623040+bb-div@users.noreply.github.com>
2023-11-24 10:28:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+// Deaktivieren des Scrollens
+document.body.classList.add("no-scroll");
+
+setTimeout(function () {
+  let loadingScreen = document.getElementById("loading-screen");
+  loadingScreen.style.opacity = "0";
+
+  setTimeout(function () {
+    loadingScreen.style.display = "none";
+    document.body.classList.remove("no-scroll");
+
+    // Fügt die fade-in Klasse zu den Header-Elementen hinzu
+    let headerContents = document.querySelectorAll(".header");
+    headerContents.forEach(function (element) {
+      element.classList.add("fade-in");
+    });
+  }, 1500); // Wartet, bis der Ladebildschirm ausgeblendet ist
+}, 3000); // Zeit, die der Ladebildschirm angezeigt wird
+
+document.addEventListener("scroll", function () {
+  const elements = document.querySelectorAll(
+    ".section-description, .section-title"
+  ); // Klasse Ihrer Elemente
+  const windowHeight = window.innerHeight;
+
+  elements.forEach(function (element) {
+    const elementPosition = element.getBoundingClientRect().top;
+    const revealPoint = 150; // Anpassbar, wie weit im Viewport das Element sein soll
+
+    if (elementPosition < windowHeight - revealPoint) {
+      element.classList.add("fade-in-view");
+    }
+  });
+});
